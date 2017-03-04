34ca8055ed6d1eeed140daacbd8aa6e3ef6130e0
caciano <oalme004@fiu.edu>
2017-03-04 14:25:20 -0500

    video tutorial part 17 complete

---
+++ models/order.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Caciano on 3/4/2017.
+ */
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+var schema = new Schema
+({
+    user: {type: Schema.Types.ObjectId, ref: "User"},
+    cart: {type: Object, required: true},
+    address: {type: String, required: true},
+    name: {type: String, required: true},
+    paymentId: {type: String, required: true}
+});
+
+module.exports = mongoose.model("Order", schema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Cart = require("../models/cart");
-
+var Order = require("../models/order");
 
 
 var Product = require("../models/product");
@@ -58,9 +58,19 @@ router.post("/checkout", (req, res, next) => {
             req.flash("error", err.message);
             res.redirect("/checkout");
         }
-        req.flash("success", "Successfully bought product");
-        req.session.cart = null;
-        res.redirect("/");
+        var order = new Order({
+            user: req.user,
+            cart: cart,
+            address: req.body.address,
+            name: req.body.name,
+            paymentId: charge.id
+        });
+        order.save(function(err, result)
+        {
+            req.flash("success", "Successfully bought product");
+            req.session.cart = null;
+            res.redirect("/");
+        });
     });
 });
 
