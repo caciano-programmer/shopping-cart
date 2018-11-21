d54df233eb87ffeefbca345d75e23b2981f870a3
caciano-programmer <caciano-programmer@users.noreply.github.com>
2018-11-21 15:58:23 -0500

    update seeder data to have right files

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -1,55 +1,52 @@
-/**
- * Created by Caciano on 2/27/2017.
- */
+var Product = require('../models/product');
 
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/db", (err) => { if(err) throw err; });
-mongoose.Promise = global.Promise;
+var mongoose = require('mongoose');
 
-var Product = require("../models/product");
-var products =
-[
+mongoose.connect('localhost:27017/shopping');
+
+var products = [
     new Product({
-        imagePath: "images/Gothiccover.png",
-        title: "Gothic Video Game",
-        description: "Awesome Game",
-        price: 13
-    }),
-    new Product({
-        imagePath: "images/dark.jpg",
-        title: "Dark Souls",
-        description: "Awesome Game",
-        price: 15
-    }),
-    new Product({
-        imagePath: "images/gears.jpg",
-        title: "Gears of war",
-        description: "Awesome Game",
-        price: 11
-    }),
-    new Product({
-        imagePath: "images/nba.jpg",
-        title: "NBA2k",
-        description: "Awesome Game",
-        price: 15
-    }),
-    new Product({
-        imagePath: "images/halo.jpg",
-        title: "Halo 5",
-        description: "Awesome Game",
-        price: 12
-    }),
-    new Product({
-        imagePath: "images/Oblivion.png",
-        title: "Oblivion",
-        description: "Awesome Game",
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: 'Gothic Video Game',
+        description: 'Awesome Game!!!!',
         price: 10
+    }),
+    new Product({
+        imagePath: 'http://eu.blizzard.com/static/_images/games/wow/wallpapers/wall2/wall2-1440x900.jpg',
+        title: 'World of Warcraft Video Game',
+        description: 'Also awesome? But of course it was better in vanilla ...',
+        price: 20
+    }),
+    new Product({
+        imagePath: 'https://support.activision.com/servlet/servlet.FileDownload?file=00PU000000Rq6tz',
+        title: 'Call of Duty Video Game',
+        description: 'Meh ... nah, it\'s okay I guess',
+        price: 40
+    }),
+    new Product({
+        imagePath: 'https://pmcdeadline2.files.wordpress.com/2014/02/minecraft__140227211000.jpg',
+        title: 'Minecraft Video Game',
+        description: 'Now that is super awesome!',
+        price: 15
+    }),
+    new Product({
+        imagePath: 'https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg',
+        title: 'Dark Souls 3 Video Game',
+        description: 'I died!',
+        price: 50
     })
 ];
 
-for(var i = 0, done = 0; i < products.length; i++, done++)
-    products[i].save( (err, result) => {
-        if(done == products.length) mongoose.disconnect();
+var done = 0;
+for (var i = 0; i < products.length; i++) {
+    products[i].save(function(err, result) {
+        done++;
+        if (done === products.length) {
+            exit();
+        }
     });
+}
 
-mongoose.disconnect();
+function exit() {
+    mongoose.disconnect();
+}
